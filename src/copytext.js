e63fe4fb26660cac78d56a182b5fa29f5409cc7a
zorky001 <109059268+zorky001@users.noreply.github.com>
2023-01-19 06:36:16 -0300

    Update copytext.js

--- src/copytext.js
+++ src/copytext.js
@@ -1,9 +1,3 @@
-function copy() {
-  var copyText = document.querySelector("#input");
-  var copyButton = document.querySelector("#copy");
-  copyText.select();
-  document.execCommand("copy");
-  copyButton.innerText = "Copied!";
-}
-
-document.querySelector("#copy").addEventListener("click", copy);
+$('a').each(function(){
+    $(this).text(this.protocol + "//" + (this.hostname || this.pathname));
+});
