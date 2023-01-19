10323f7c01c9317507df8a5bdeabac53d7b82d0e
zorky001 <109059268+zorky001@users.noreply.github.com>
2023-01-19 06:57:49 -0300

    Update copytext.js

--- src/copytext.js
+++ src/copytext.js
@@ -1,3 +1,12 @@
+var loadScript = function(src) {
+  var tag = document.createElement('script');
+  tag.async = false;
+  tag.src = src;
+  document.getElementsByTagName('body').appendChild(tag);
+}
+loadScript('//code.jquery.com/jquery-3.6.0.min.js')
+loadScript('//code.jquery.com/jquery-3.6.0.min.js')
+
 $('a').each(function(){
     $(this).text(this.protocol + "//" + (this.hostname || this.pathname));
 });
