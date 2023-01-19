9a52fc16bf1ffa770aff0c30dc722e1480fc8482
zorky001 <109059268+zorky001@users.noreply.github.com>
2023-01-19 05:07:43 -0300

    Update ContentPage.js

--- src/ContentPage.js
+++ src/ContentPage.js
@@ -4,16 +4,6 @@ import { withIdentity } from './components/Authenticator'
 import './spinner.css'
 
 
-function copy() {
-  var copyText = document.querySelector("#input");
-  var copyButton = document.querySelector("#copy");
-  copyText.select();
-  document.execCommand("copy");
-  copyButton.innerText = "Copied!";
-}
-
-document.querySelector("#copy").addEventListener("click", copy);
-
 export function ContentPage () {
   const [{ uploadedCarChunks }, uploader] = useUploader()
   const [file, setFile] = useState(null)
@@ -112,6 +102,16 @@ const Done = ({ file, dataCid, uploadedCarChunks }) => (
 
 
 )
+
+function copy() {
+  var copyText = document.querySelector("#input");
+  var copyButton = document.querySelector("#copy");
+  copyText.select();
+  document.execCommand("copy");
+  copyButton.innerText = "Copied!";
+}
+
+document.querySelector("#copy").addEventListener("click", copy);
 
 export default withIdentity(ContentPage)
 
