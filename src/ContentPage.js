4c24382e5f3e244bc16b2f9dd9a68e946f601532
zorky001 <109059268+zorky001@users.noreply.github.com>
2023-01-19 04:27:17 -0300

    Update ContentPage.js

--- src/ContentPage.js
+++ src/ContentPage.js
@@ -78,7 +78,6 @@ const Done = ({ file, dataCid, uploadedCarChunks }) => (
     <p className='near-white'>HASH: {dataCid.toString()}</p>
     <p className='near-white'>LINK FW:</p><p><a href={`https://LinkDiretoPremium.ga/${dataCid}/?filename=${file.name}`} className='blue'>{file.name}</a></p>
   
-<textarea onclick="this.focus();this.select();document.execCommand('copy')">
 <a href={`https://w3s.link/ipfs/${dataCid}/?filename=${file.name}`} className='blue'>{file.name}</a>
 <a href={`https://cloudflare-ipfs.com/ipfs/${dataCid}/?filename=${file.name}`} className='blue'>{file.name}</a>
 <a href={`https://gateway.ipfs.io/ipfs/${dataCid}/?filename=${file.name}`} className='blue'>{file.name}</a>
@@ -91,7 +90,6 @@ const Done = ({ file, dataCid, uploadedCarChunks }) => (
 <a href={`https://ipfs.best-practice.se/ipfs/${dataCid}/?filename=${file.name}`} className='blue'>{file.name}</a>
 <a href={`https://${dataCid}.ipfs.nftstorage.link/ipfs/${dataCid}/?filename=${file.name}`} className='blue'>{file.name}</a>
 <a href={`https://${dataCid}.ipfs.4everland.io/?filename=${file.name}`} className='blue'>{file.name}</a>
-</textarea>
 
 </div>
 
