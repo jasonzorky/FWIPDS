060acbf8568441092be5e3ae1ca7757f37c894e9
zorky001 <109059268+zorky001@users.noreply.github.com>
2023-01-19 13:52:50 -0300

    Update ContentPage.js

--- src/ContentPage.js
+++ src/ContentPage.js
@@ -97,13 +97,13 @@ const Done = ({ file, dataCid, uploadedCarChunks }) => (
 <a href={`https://${dataCid}.ipfs.4everland.io/?filename=${file.name}`} className='blue'>https://{dataCid.toString()}.ipfs.4everland.io/?filename={file.name}</a>{'\n'}
 {'\n'}
 {'\n'}
-
-
-
 </div>
+
+<div>
 {'\n'}
 {'\n'}
 {'\n'}
+</div>
 )
 
 
