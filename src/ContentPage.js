fa2883abe74b9c5f7348b1e5a01e3dd8d6d28d63
zorky001 <109059268+zorky001@users.noreply.github.com>
2023-01-19 11:19:08 -0300

    Update ContentPage.js

--- src/ContentPage.js
+++ src/ContentPage.js
@@ -79,7 +79,8 @@ const Done = ({ file, dataCid, uploadedCarChunks }) => (
     <h1 className='near-white'>UPLOAD COMPLETO!</h1>
     <p className='near-white'>HASH: {dataCid.toString()}</p>
     <p className='near-white'>LINK FW:</p><p>
-<a href={`https://LinkDiretoPremium.ga/${dataCid}/?filename=${file.name}`} className='blue'>{url.href}</a></p>
+    let url="https://LinkDiretoPremium.ga/${dataCid}/?filename=${file.name}";
+<a href={`https://LinkDiretoPremium.ga/${dataCid}/?filename=${file.name}`} className='blue'>{url}</a></p>
 <a href={`https://w3s.link/ipfs/${dataCid}/?filename=${file.name}`} className='blue'>{file.name}</a>
 <a href={`https://cloudflare-ipfs.com/ipfs/${dataCid}/?filename=${file.name}`} className='blue'>{file.name}</a>
 <a href={`https://gateway.ipfs.io/ipfs/${dataCid}/?filename=${file.name}`} className='blue'>{file.name}</a>
@@ -92,11 +93,12 @@ const Done = ({ file, dataCid, uploadedCarChunks }) => (
 <a href={`https://ipfs.best-practice.se/ipfs/${dataCid}/?filename=${file.name}`} className='blue'>{file.name}</a>
 <a href={`https://${dataCid}.ipfs.nftstorage.link/ipfs/${dataCid}/?filename=${file.name}`} className='blue'>{file.name}</a>
 <a href={`https://${dataCid}.ipfs.4everland.io/?filename=${file.name}`} className='blue'>{file.name}</a>
+
+
 </div>
 )
 
-const url = new URL('https://LinkDiretoPremium.ga/${dataCid}/?filename=${file.name}');
-url.href; // => 'http://example.com/path/index.html'
+
 
 export default withIdentity(ContentPage)
 
