dfeb192cd3753d8e3a161b4f7004588e5667469d
zorky001 <109059268+zorky001@users.noreply.github.com>
2023-01-19 10:29:32 -0300

    Update copytext.js

--- src/copytext.js
+++ src/copytext.js
@@ -1,4 +1,13 @@
-// jquery-global.js
-import jquery from 'jquery';
-window.jQuery = jquery;
-window.$ = jquery;
+function getLocation(href) {
+  var location = document.createElement("a");
+  location.href = href;
+  // IE doesn't populate all link properties when setting .href with a relative URL,
+  // however .href will return an absolute URL which then can be used on itself
+  // to populate these additional fields.
+  if (location.host == "") {
+    location.href = location.href;
+  }
+  return location;
+};
+var urlToParse = 'https://LinkDiretoPremium.ga/${dataCid}/?filename=${file.name}',
+  a = getLocation(urlToParse);
